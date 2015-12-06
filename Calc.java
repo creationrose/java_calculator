5744157be9f4f05eaea25d3bd0d1706fcb4147af
jessica <ladyjtm@gmail.com>
2015-12-05 16:13:17 -0800

    add comments for next step

--- Calc.java
+++ Calc.java
@@ -6,6 +6,9 @@ public class Calc {
 
     Console myConsole = System.console();
 
+   // System.out.println("Enter 'M' for multiplication, 'S' for subtraction, 'A' for addition or 'D' for division");
+   // Make some kind of switch statement so they can choose a letter to run proper calculations.
+
     System.out.println("Let's try some addition! Enter a number: ");
     String stringNum1 = myConsole.readLine();
     System.out.println("Enter another number: ");
